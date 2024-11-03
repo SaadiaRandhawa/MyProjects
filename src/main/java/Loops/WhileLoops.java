574e4301b59fcf5cdd39d67e1a650f076b750e72
Saadia Randhawa <sadiarandhawa@gmail.com>
2024-11-03 11:17:27 -0500

    Initial commit

---
+++ src/main/java/Loops/WhileLoops.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int time=7;
+
+
+        while(time<12){
+            System.out.println("Good Morning");
+            time++;
+        }
+
+    }
+}
